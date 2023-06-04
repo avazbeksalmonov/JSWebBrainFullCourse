e7a0853e348889c07c1f280370a633faef995448
Avazbek <avazbeksalmonov1998@mail.ru>
2023-06-04 09:21:36 +0500

    add new js file

---
+++ 2-dars.TypeConversion/app.js
@@ -0,0 +1,35 @@
+// let a = "5";
+// console.log(typeof a); //string
+// let b = 5;
+// console.log(typeof toString(b)); //Numberni stringga o'tqizib beradi
+
+// let c = 5;
+// console.log(isNaN(c)); // false
+
+// let c2 = "12c";
+// console.log(isNaN(c2)); //true
+
+//================
+
+//true = !0, !null, !undefined;
+
+// console.log(false == 0); //true
+// console.log(false == 0); //true
+// console.log(true == 1); //true
+// console.log(true == null); //false
+// console.log(false == null); //false
+// console.log(null == undefined); // true
+
+// console.log(true == "1"); //true
+// console.log(true == "a"); //false
+// console.log(true == "2"); //false
+// console.log(true == 1); //false
+// console.log(false == "2"); // false
+// console.log(false == 0); //true
+// console.log(false == 2); //fasle
+// ================= inc,decr --,++
+
+// let a = 1;
+// //
+// console.log(a++);
+// console.log(a);

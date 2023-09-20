8201d5a87b9a6e7e9eaa416d0e50aa34d4bd62e7
Avazbek <avazbeksalmonov1998@mail.ru>
2023-09-20 14:22:41 +0500

    add new js code

--- 19-dars.AdvancedFunctions/app.js
+++ 19-dars.AdvancedFunctions/app.js
@@ -1,11 +1,63 @@
-let btn = document.querySelector("button");
+// Function *** call(),apply(),bind()
+let user = {
+  name: "Webbrain",
+  lastName: "Academy",
+  title: "IT Center",
+  getFull() {
+    console.log(`${this.name} ${this.lastName}`);
+  },
+};
+let user2 = {
+  name: "Digital",
+  lastName: "One",
+  title: "IT Center",
+  getFull() {
+    console.log(`${this.name} ${this.lastName}`);
+  },
+};
+user.getFull();
+user2.getFull();
 
-btn.addEventListener("click", () => {
-  clearInterval(timeout);
-  console.log("Tugadi Interval");
-});
+let users = [
+  {
+    name: "Webbrain",
+    lastName: "Academy",
+    title: "IT Center",
+  },
+  {
+    name: "Digital",
+    lastName: "One",
+    title: "IT Center",
+  },
+];
 
-let timeout = setInterval(() => {
-  console.log("Timeout ishladi");
-  alert("Interval ishlayapdi");
-}, 3000);
+function getFull(a, b) {
+  console.log(a, b);
+  //   console.log(this);
+  console.log(`${this.name} ${this.lastName}`);
+}
+for (let usr of users) {
+  getFull.call(usr, "eshmat", "toshmat");
+  getFull.apply(usr, ["eshmat", "toshmat"]);
+}
+// call va apply farqi parametr qabul qilishida
+// apply array ko 'rinishda qabul qiladi
+
+getFull.call(user);
+getFull({ name: "webbrain", lastName: "academy" });
+// getFull.call(contex,par)
+
+// ** bind() funksiya qaytaradi **
+function getFull(a) {
+  console.log(a);
+  console.log(`${this.name} ${this.lastName}`);
+}
+let mt = getFull.bind(user, "hey");
+mt();
+// getFull.bind()();
+
+const getData = () => {
+  return fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+    res.json()
+  );
+};
